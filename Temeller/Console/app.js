0567a892b435f1a4dab2dc63ecc4534f4961fa83
Asfeanor <altugegesari@gmail.com>
2020-05-08 20:15:25 +0300

    ucuncu

---
+++ Temeller/Console/app.js
@@ -0,0 +1,15 @@
+// console.log("Hello Javascript");
+// console.log(213213);
+// console.log(true);
+// console.log(false);
+//
+// var a=20;
+// console.log(a);
+//
+// console.log([`Ahmet`,`Mehmet`]);
+//
+// console.log(typeof a);
+
+console.warn("This is a Warning!");
+console.error("This page cannot find!");
+console.clear()
---
+++ Temeller/Variables and data types/app.js
@@ -0,0 +1,51 @@
+// Creating a variable
+
+// var a = 20;
+// var b = 10;
+// var c = 40;
+//
+// console.log(a,b,c);
+
+// Primitive
+
+// var a = 10; // Number Data Type
+// var b = 3/14;
+// console.log(typeof a);
+// console.log(typeof b);
+
+//String
+
+// var name = "Mustafa";
+// console.log(name);
+// console.log(typeof name);
+
+// Boolean
+
+// var a = true;
+//
+// console.log(typeof a)
+
+// var a = null;
+// console.log(a);
+// console.log(typeof a);
+
+// var a;
+// console.log(a);
+
+// Referance
+
+// var numbers = [1,2,3,4,5];
+// // console.log(numbers);
+// // console.log(typeof numbers);
+// // console.log(numbers[0]);
+
+// var date = new Date();
+//
+// console.log(date);
+// console.log(typeof date);
+
+// var merhaba = function () {
+//     console.log("Merhaba");
+// }
+// console.log(merhaba);
+// console.log(typeof merhaba);
