4d2cc819f465ceb63d34a6acf920ea686600bc3e
Asfeanor <altugegesari@gmail.com>
2020-05-15 18:56:51 +0300

    Date object methods

---
+++ Temeller/Date Object/app.js
@@ -0,0 +1,15 @@
+let value;
+
+const now = new Date(); //Current time
+
+const date1 = new Date("6-15-1999 02:00:00");
+
+value = date1;
+
+value = date1.getMonth();
+value = date1.getDate();
+value = date1.getDate();
+value = date1.getHours();
+
+
+console.log(value);
