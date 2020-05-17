89bf793dd76c0ba5b0c3106be6dda87b3fee9d25
Asfeanor <altugegesari@gmail.com>
2020-05-17 14:06:19 +0300

    Conditions

---
+++ Temeller/Comparsion Operators/app.js
@@ -0,0 +1,64 @@
+// Comparsion operators
+
+ // ==
+ // ===
+ // !=
+ // !==
+ // >
+ // <
+ // >=
+ // <=
+
+// console.log(2 == 2);
+// // console.log("js" == "java");
+// // console.log(2 == "2");
+// // console.log(2 === "2");
+// //
+// // console.log(4 > 2);
+
+// console.log(2 != 4);
+
+// Logical Conjunctions
+
+// Not operator
+
+// console.log(!(2 != 2));
+
+// And operator
+
+// console.log((2 == 2) && ("Ahmet" == "Ahmet"));
+
+// Or operator
+
+// const error = false;
+
+// if (error == true){
+//     console.log("An error occurred!");
+// }
+// else{
+//     console.log("No error occurred!");
+// }
+
+// const process = "1";
+//
+// if (process === "1"){
+//     console.log("The process is 1.");
+// }
+//
+// else if (process === "2"){
+//     console.log("The process is 2.");
+// }
+//
+// else if (process === "3"){
+//     console.log("The process is 3.");
+// }
+//
+// else if (process === "4"){
+//     console.log("The process is 4.");
+// }
+//
+// else{
+//     console.log("Invalid processing.");
+// }
+
+
