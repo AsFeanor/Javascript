d946a85b2b051350f12cf104d426099fd0684e2a
Asfeanor <altugegesari@gmail.com>
2020-05-20 19:06:18 +0300

    window object

---
+++ Temeller/Window Object/app.js
@@ -0,0 +1,51 @@
+// alert
+
+// alert("Hello");
+
+// const cevap = confirm("Are you sure_");
+//
+// console.log(cevap);
+//
+// if (cevap){
+//     console.log("delete this");
+// }
+// else{
+//     console.log("Do not delete");
+// }
+
+// const cevap = prompt("2 + 2 = ?");
+//
+// if (cevap == "4"){
+//     console.log("True");
+// }
+// else{
+//     console.log("False");
+// }
+
+let value;
+
+value = window;
+value = window.location;
+value = window.location.host;
+value = window.location.hostname;
+value = window.location.port;
+value = window.location.href;
+
+// if (confirm("Refresh the Page?")){
+//     window.location.reload();
+// }
+// else {
+//     console.log("The Page Was Not Refreshed.");
+// }
+
+
+value = window.outerHeight;
+value = window.outerWidth;
+
+value = window.innerHeight;
+value = window.innerWidth;
+
+value = window.scrollX;
+value = window.scrollY;
+
+console.log(value);
