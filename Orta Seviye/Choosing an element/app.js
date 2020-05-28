9d269d314342b0ea4454007d44264e5d785dcf42
Asfeanor <altugegesari@gmail.com>
2020-05-28 21:08:00 +0300

    Dom Manipulation (2)

---
+++ Orta Seviye/Choosing an element/app.js
@@ -0,0 +1,13 @@
+// Selecting element by ID
+
+let element;
+
+element = document.getElementById("todo-form");
+element = document.getElementById("tasks-title");
+
+// Selecting element by Class
+
+element = document.getElementsByClassName("list-group-item")[0];
+
+
+console.log(element);
---
+++ Orta Seviye/Selecting element and changing the style specs/app.js
@@ -0,0 +1,44 @@
+const element = document.querySelector("#clear-todos");
+
+// element specs
+
+
+// console.log(element.id);
+// console.log(element.className);
+// console.log(element.classList);
+// console.log(element.classList[1]);
+// console.log(element.textContent);
+// console.log(element.innerHTML);
+// console.log(element.href);
+// console.log(element.style);
+
+// Changing Style and Element specs
+
+// element.className = "btn btn-primary";
+// element.style.color = "#000";
+// element.style.marginLeft = "50px";
+// element.href = "https://www.google.com.tr";
+// element.target = "_blank"
+// element.textContent = "Delete";
+// element.innerHTML = "<span style='color: green'>Delete</span>"
+
+let element2 = document.querySelector("li:last-child");
+element2 = document.querySelector("li:nth-child(2)");
+element2 = document.querySelector("li:nth-child(3)");
+element2 = document.querySelector("li:nth-child(4)");
+element2 = document.querySelectorAll("li:nth-child(odd)");
+element2 = document.querySelectorAll("li:nth-child(even)");
+
+element2.forEach(function (el) {
+    el.style.background = "#ccc";
+    el.style.color = "red";
+
+})
+// const elements = document.querySelectorAll(".list-group-item");
+// elements.forEach(function (el) {
+//     el.style.color = "red";
+//     el.style.background = "#eee";
+// });
+
+console.log(element2);
+// console.log(element);
