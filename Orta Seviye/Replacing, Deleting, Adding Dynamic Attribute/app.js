29cd5c6e7435746ed81a8e49c0cd00519c97cb4b
Asfeanor <altugegesari@gmail.com>
2020-06-05 22:36:01 +0300

    Replacing, Deleting, Adding Dynamic Attirute

---
+++ Orta Seviye/Replacing, Deleting, Adding Dynamic Attribute/app.js
@@ -0,0 +1,22 @@
+const todoInput = document.getElementById("todo");
+let element;
+
+
+element = todoInput;
+element = todoInput.classList;
+
+// todoInput.classList.add("newClass");
+// todoInput.classList.add("newClass2");
+// todoInput.classList.remove('form-control');
+
+element = todoInput
+element = todoInput.placeholder;
+element = todoInput.getAttribute("placeholder");
+todoInput.setAttribute("placeholder", "What's up");
+todoInput.setAttribute("title", 'Input');
+todoInput.removeAttribute("name");
+element = todoInput;
+// element = todoInput.hasAttribute('name');
+
+
+console.log(element);
