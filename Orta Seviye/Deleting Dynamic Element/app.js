7dfe481dbca37b3f9ab46171c69e50b31a25c760
Asfeanor <altugegesari@gmail.com>
2020-06-05 22:18:40 +0300

    Deleting and Replacing Dynamic Elements

---
+++ Orta Seviye/Deleting Dynamic Element/app.js
@@ -0,0 +1,17 @@
+// Deleting Dynamic Element
+
+const todoList = document.querySelector("ul.list-group");
+const todos = document.querySelectorAll("li.list-group-item");
+
+// Remove Method
+
+// todos[1].remove();
+
+// Remove Child
+
+// todoList.removeChild(todoList.lastElementChild);
+// todoList.removeChild(todos[3]);
+
+
+console.log(todos);
+console.log(todoList);
